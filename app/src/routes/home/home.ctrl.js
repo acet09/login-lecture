6a9394cdf3cc8a8b3ea49dbae35bb27cd0f14033
Cactus <ysjungman@gmail.com>
2023-02-07 12:02:18 +0900

    벡앤드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,40 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
-}
-const login = (req, res) => {
-  res.render("home/login");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
+};
+
+const users = {
+  id: ["coco", "게발맨", "오타쟁이"],
+  psword: ["123", "123", "1234"],
 }
 
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다.",
+    });
+  },
+};
+
 module.exports = {
-  home,
-  login
+  output,
+  process,
 }
