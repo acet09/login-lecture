b1fceab5e2d6060f67d73600d43a037223ec8c2f
Cactus <ysjungman@gmail.com>
2023-02-08 10:52:21 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,24 +9,27 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener('click', register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력 해주세요.");
+  
+  if (psword.value !== confirmPsword.value)return alert("비밀번호가 일치하지 않습니다.");
+  
+
   const req = {
     id: id.value,
     name: name.value,
     psword: psword.value,
-    confirmPsword: confirmPsword.value,
   };
-  console.log(req);
 
-  fetch("/register",{
+  fetch("/register", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", 
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
     .then((res) => {
-      if(res.success) {
+      if (res.success) {
         location.href = "/login";
       } else {
         alert(res.msg);
